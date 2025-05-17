221cad1912a0c2fa10b55a40a80970430d1a263e
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2025-05-17 13:02:17 +0300

    header done

--- src/components/main-wrapper/header/Header.tsx
+++ src/components/main-wrapper/header/Header.tsx
@@ -1,10 +1,27 @@
 import { menu } from "motion/react-client";
 import { Menu } from "./Menu";
+import { Bell, Grip, Search } from "lucide-react";
+import Image from "next/image";
 
 export function Header() {
   return (
-    <header className="p-7">     
-      <Menu />
+    <header className="p-7 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <Grip className="transition-colors hover:text-primary"/>     
+        <Menu />
+      </div>
+
+      <div className="flex items-center gap-6">
+        <Search className="transition-colors hover:text-primary"/>
+        <Bell className="transition-colors hover:text-primary"/>
+        <Image 
+          src="/avatar-killflex.jpg"
+          alt="Profile picture"
+          width={40}
+          height={40} 
+          className="rounded-full"
+        />          
+      </div>      
     </header>
   )
 }
