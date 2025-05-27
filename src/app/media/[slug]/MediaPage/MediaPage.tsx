ed62e18b8382a27e26c530c808e055b3bf21d516
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2025-05-27 19:52:21 +0300

    Products DONE

--- src/app/media/[slug]/MediaPage/MediaPage.tsx
+++ src/app/media/[slug]/MediaPage/MediaPage.tsx
@@ -10,9 +10,20 @@ import { CSSProperties } from "react";
 export function MediaPage({mediaItem}: IMediaPage) {
     const {
     props: { srcSet },
-  } = getImageProps({ alt: '', width: 1282, height: 540, src: mediaItem.backdrop });
+  } = getImageProps({
+    alt: '',
+    width: 1282,
+    height: 540,
+    src: mediaItem.backdrop,
+    priority: true,
+    quality: 100
+ });
   const backgroundImage = getBackgroundImage(srcSet)
-  const style:CSSProperties = { height: 540, width: '100%', backgroundImage }
+  const style: CSSProperties = {
+    height: 540,
+    width: '100%',
+    backgroundImage
+  }    
 
     return (
     <div>
--- src/app/media/[slug]/MediaPage/Products.tsx
+++ src/app/media/[slug]/MediaPage/Products.tsx
@@ -3,23 +3,25 @@ import { ChevronRight } from "lucide-react";
 
 export function Products() {
     return (
-        <div className="bg-black/50 p-2 rounded-lg">                
-            <div className="relative">
-                <div className="absolute w-full h-full bg-white -top-10 left-2 scale-80"/>
-                <div className="absolute w-full h-full bg-white -top-7 left-2 scale-90"/>
+        <div className="relative">                
+            <div className="absolute left-2.5 bottom-10">
                 <Image
-                    width={200}
-                    height={200}
+                    width={180}
+                    height={180}
                     src="/products/product.png"
                     alt="product"
-                    className="bg-white rounded-lg"
+                    className="bg-white rounded-lg z-3 relative"
                 />
+                <div className="absolute w-full h-full bg-white/90 rounded-lg -top-5 left-0.5 scale-90 z-2"/>
+                <div className="absolute w-full h-full bg-white/80 rounded-lg -top-9.5 left-1 scale-80 z-1"/>
             </div>
-            <div className="flex justify-between items-center">
-                <button className="text-secondary font-semibold">
-                    Shop now
-                </button>
-                <ChevronRight className="text-secondary"/>
+            <div className="bg-black/50 px-4 py-2.5 rounded-lg h-28 w-[200px] flex items-end">
+                <div className="flex justify-between items-center mt-2 w-full text-sm">
+                    <button className="text-secondary font-semibold">
+                        Shop now
+                    </button>
+                    <ChevronRight className="text-secondary" size={20}/>
+                </div>
             </div>
         </div> 
     );
